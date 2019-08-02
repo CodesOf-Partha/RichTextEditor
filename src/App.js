95562a35d789239c209117021ea9c8cd0e4d82e1
p0rth01998 <rvparthasarathi@gmail.com>
2019-08-02 15:01:33 +0530

    updated Commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import{EditorController} from "./EditorController";
 import {Editor, EditorState, RichUtils} from 'draft-js';
+import {stateFromHTML} from 'draft-js-import-html';
+import {stateToHTML} from 'draft-js-export-html';
+const initialState="Type here Something!!!";
 class App extends React.Component {
   state={
     title:"",
-    editorState:EditorState.createEmpty()
+    editorState:EditorState.createWithContent(stateFromHTML(initialState))
   };
 titleChange=event=>{this.setState({title:event.target.value})}
 onEditorchange=(editorState)=>this.setState({editorState});
+submitInput=()=>{
+  console.log(stateToHTML(this.state.editorState.getCurrentContent()));
+}
 toggleInlineStyle=inlineStyle=>{
   this.setState({
     editorState:RichUtils.toggleInlineStyle(
@@ -24,7 +30,7 @@ toggleInlineStyle=inlineStyle=>{
       <h1>{this.state.title}</h1>
      <EditorController editorState={this.state.editorState} toggleInlineStyle={this.toggleInlineStyle}/>
      <Editor editorState={this.state.editorState} onChange={this.onEditorchange} />
-
+      <button className="btn bg-primary" onClick={this.submitInput}>Submit</button>
    </div> 
   );
 }
